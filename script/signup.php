9dc160c3115e7bdb8ca75d32839dd396198a84ae
Cristian Andrei <andcrist246@yahoo.com>
2018-05-09 21:24:18 +0300

    create users table
    plus creating the admin for the site

--- script/signup.php
+++ script/signup.php
@@ -1,16 +1,16 @@
 <?php
+require('db.php');
 session_start();
 
 $username = "";
 $password = "";
+$email = "";
 $errors = array();
 
-$db=mysqli_connect('speculapp.000webhostapp.com','id5658853_speculapp_user','12345678','id5658853_speculapp');
-
 if (isset($_POST['reg_user'])) {
-	$username = mysqli_real_escape_string($db, $_POST['username']);
-	$email = mysqli_real_escape_string($db, $_POST['email']);
-	$password = mysqli_real_escape_string($db, $_POST['password']);
+	$username = mysqli_real_escape_string($con, $_POST['username']);
+	$email = mysqli_real_escape_string($con, $_POST['email']);
+	$password = mysqli_real_escape_string($con, $_POST['password']);
 	$encrypt_password = md5($password);
 
 	if (preg_match('!@#$%^&*()[]{};\',<.>/? -', $username)) {
@@ -21,7 +21,7 @@ if (isset($_POST['reg_user'])) {
 	$email=stripslashes($email);
 
 	$user_check_query = "SELECT * FROM users WHERE username='$username' OR email='$email' LIMIT 1";
-	$result = mysqli_query($db,$user_check_query);
+	$result = mysqli_query($con,$user_check_query);
 	$user = mysqli_fetch_assoc($result);
 
 	if ($user){
@@ -37,11 +37,11 @@ if (isset($_POST['reg_user'])) {
 	if (count($errors)==0){
 		$encrypt_password = md5($password);
 		$date = date('Y-m-d H:i:s');
-		$query = "INSERT INTO user (username, password, email, role, created_at, updated_at) VALUES ('$username','$encrypt_password', '$email', 'user', '$date', '$date')";
-		mysqli_query($db,$query);
+		$query = "INSERT INTO users (username, password, email, role, created_at, updated_at) VALUES ('$username','$encrypt_password', '$email', 'user', '$date', '$date')";
+		mysqli_query($con,$query);
 		$_SESSION['username'] = $username;
-		$_SESSION['success'] "You are now logged in.";
-		header("location:login_success.php");
+		$_SESSION['success'] = "You are now logged in.";
+		header("location:../page/Client.html");
 	}
 }
 ?>
