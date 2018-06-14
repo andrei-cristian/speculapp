b302c189461a3b68e0881fbcc93de753234daf45
Cristian Andrei <andcrist246@yahoo.com>
2018-06-14 15:09:08 +0300

    made about.php get from database the values

--- page/About.php
+++ page/About.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
 require ('../script/session_check.php');
+include ('../script/include.php');
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -20,12 +21,12 @@ require ('../script/session_check.php');
 </ul>
 <div class="reg">
 <h2 style="text-align: center">Regulations</h2>
-    <p>1.To start, you must deposit at least 3000RON.</p>
+    <p>1.To start, you must deposit at least <?php echo mysqli_fetch_assoc(get_game_data())['init_money']?>RON.</p>
     <p>2.The first change you make won't affect the total value. That will be the base for your next moves.</p>
     <p>3.Using the exchange rate table, make decisions. Keep in mind that the rates can change anytime.</p>
-    <p>4.Once your total value is over 6000RON, you win.</p>
-    <p>5.If yout total value is under 100RON, you lose.</p>
-    <p>6.To play another game, you should deposit what is left to 1000RON.</p>
+    <p>4.Once your total value is over <?php echo mysqli_fetch_assoc(get_game_data())['win_limit']?>RON, you win.</p>
+    <p>5.If yout total value is under <?php echo mysqli_fetch_assoc(get_game_data())['lose_limit']?>RON, you lose.</p>
+    <p>6.To play another game, you should deposit what is left to <?php echo mysqli_fetch_assoc(get_game_data())['init_money']?>RON.</p>
 </div>
 </body>
 </html>
