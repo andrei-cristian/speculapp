38dc9fcde22f6796c65db647b04226601e705190
malinac <malina.catanoiu@gmail.com>
2018-06-14 14:14:24 +0300

    done

--- page/About.php
+++ page/About.php
@@ -20,10 +20,10 @@ require ('../script/session_check.php');
 </ul>
 <div class="reg">
 <h2 style="text-align: center">Regulations</h2>
-    <p>1.To start, you must deposit at least 1000RON.</p>
+    <p>1.To start, you must deposit at least 3000RON.</p>
     <p>2.The first change you make won't affect the total value. That will be the base for your next moves.</p>
     <p>3.Using the exchange rate table, make decisions. Keep in mind that the rates can change anytime.</p>
-    <p>4.Once your total value is over 2000RON, you win.</p>
+    <p>4.Once your total value is over 6000RON, you win.</p>
     <p>5.If yout total value is under 100RON, you lose.</p>
     <p>6.To play another game, you should deposit what is left to 1000RON.</p>
 </div>
